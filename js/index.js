1db13319b8ee04d7a68bcaaefc174df8554b6b99
Hoan07 <nhhoan07@gmail.com>
2017-09-19 15:15:52 -0400

    Finish javascript part. Checked function. Good. Checked responsive for many sizes screen. Good.

--- js/index.js
+++ js/index.js
@@ -1,51 +1,71 @@
-function addIconSearch() {
-  return '<button class="btn btn-link" onclick="iconSearchClick()"><i class="fa fa-search" style="font-size:20px;"></i></button>';
-}
-function addSearchInput() {
-  return '<div style="margin:auto 20% auto 20%" class="input-group"><input id="searchBox" type="text" class="form-control" placeholder="Search for ..."><span class="input-group-btn"><button class="btn btn-light" onclick="iconClearClick()"><i class="fa fa-remove"></i></button><button class="btn btn-light" onclick="iconSearchResult()"><i class="fa fa-search"></i></button></span></div>';
-}
-function getRandomTopic() {
-  var library=["love","football","Bill Gates"];
-  var num = Math.floor(Math.random()*library.length);
-  return library[num];
-}
+var hisTxt = [];
+var curTxt = [];
 
-$("#searchDiv").append(addIconSearch());
-function iconClearClick() {
-  $("#searchDiv").empty();
-  $("#searchDiv").append(addIconSearch());
-  $("#searchSpace").empty();
-  $("#parent").removeClass("div11").addClass("div1");
-}
-function iconSearchClick() {
-  $("#searchDiv").empty();
-  $("#searchDiv").append(addSearchInput());
-}
-function htmlResult(val) {
-  var result='';
-  for (var i=0;i<val.length;++i) {
-    result += '<a href=https://en.wikipedia.org/?curid='+val[i][2]+'><span style="display: block;"><div class="row divtag"><div style="margin:auto 10px auto 20px" class="row text-left"><p><b>'+val[i][0]+'</b></p></div><div style="margin:auto 10px auto 20px" class="row text-left"><p>'+val[i][1]+'</p></div></div></span></a>';    
+function buttonClick(val) {
+  switch (val) {
+    case 'AC':
+      curTxt = [];
+      hisTxt = [];
+      $('#inputTxt').text('0');
+      $('#hisTxt').text('0');
+      break;
+    case 'CE':
+      curTxt = [];
+      $('#inputTxt').text('0');
+      break;
+    case '=':
+      var t = calculation(curTxt).toString();
+      $('#inputTxt').text(t);
+      curTxt = [];
+      hisTxt = t.split('');
+      $('#hisTxt').text(hisTxt.join(''));
+      break;
+    default:
+      if (curTxt.length === 0) {
+        if (val === '+' || val === '-' || val === '*' || val === "/") {
+          curTxt = hisTxt;
+        }
+      }
+      curTxt.push(val);
+      $('#inputTxt').text(curTxt.join(''));
   }
-  return result;
 }
-function showSearchResult(topic) {
-  $.getJSON("https://en.wikipedia.org/w/api.php?action=query&list=search&srsearch="+topic+"&srlimit=20&format=json"+"&callback=?", function(json) {
-    $("#searchSpace").empty();
-    var result=[];
-    for(var i=0;i<json.query.search.length;++i) {
-     result.push([json.query.search[i].title,json.query.search[i].snippet,json.query.search[i].pageid]);
+
+function calculation(arr) {
+  var val = 0;
+  var numStr = [[]];
+  var operator = [];
+
+  for (var i = 0, n = arr.length; i < n; i++) {
+    if (arr[i] === '+' || arr[i] === '-' || arr[i] === '*' || arr[i] === '/') {
+      operator.push(arr[i]);
+      numStr.push([]);
+    } else {
+      numStr[numStr.length - 1].push(arr[i]);
     }
-    
-    $("#searchSpace").append(htmlResult(result));
-  }); //end getJSON
+  }
+
+  numStr[0].length === 0? val = 0: val = parseFloat(numStr[0].join(''));
+
+  for (var j = 0, m = operator.length; j < m; j++) {
+    val = compute(val, operator[j], parseFloat(numStr[j + 1].join('')));
+  }
+
+  return val;
 }
 
-function randomSearch() {
-  showSearchResult(getRandomTopic());
-  $("#parent").removeClass("div1").addClass("div11");
-}
-
-function iconSearchResult() {
-  $("#parent").removeClass("div1").addClass("div11");
-  showSearchResult($('#searchBox').val());  
+function compute(val1, op, val2) {
+  switch (op) {
+    case '+':
+      return val1 + val2;
+    case '-':
+      return val1 - val2;
+    case '*':
+      return val1 * val2;
+    case '/':
+      return val1 / val2;
+    default:
+      return 0;
+  }
+  return 0;
 }
